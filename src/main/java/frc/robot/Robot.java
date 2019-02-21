df0a47b7e8609dfa60c26e9957143f67290ca632
Kevin <kevinryan.sw@gmail.com>
2019-02-21 16:08:44 -0500

    Unified command starts

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,14 +4,11 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-//import frc.robot.commands.DriveSkateBot;
 import frc.robot.commands.DriveWithJoysticks;
 import frc.robot.commands.MoveElevator;
 //import frc.robot.commands.RunWrist;
@@ -25,116 +22,66 @@ import frc.robot.commands.MoveElevator;
  */
 public class Robot extends TimedRobot {
   static OI oi = new OI();
-
+  static boolean commandsStarted = false;
 
   // Command m_autonomousCommand;
   // SendableChooser<Command> m_chooser = new SendableChooser<>();
   Command m_DriveWithJoysticks;
   Command m_MoveElevator;
   Command m_RunWrist;
-  
-   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
+
   @Override
   public void robotInit() {
-    // m_chooser.setDefaultOption("Default Auto", new DriveWithJoysticks());
-    // chooser.addOption("My Auto", new MyAutoCommand());
-    // SmartDashboard.putData("Auto mode", m_chooser);
-
-    // Competition and Practive Bot
     m_DriveWithJoysticks = new DriveWithJoysticks();
-    // SKATEBOT
-    //m_DriveWithJoysticks = new DriveSkateBot();
-   
     m_MoveElevator = new MoveElevator();
-    //m_RunWrist = new RunWrist();
- 
+    // m_RunWrist = new RunWrist();
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
   }
 
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
-   */
   @Override
   public void disabledInit() {
   }
 
   @Override
   public void disabledPeriodic() {
-    Scheduler.getInstance().run();
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
-   */
   @Override
   public void autonomousInit() {
-    // m_autonomousCommand = m_chooser.getSelected();
-    // schedule the autonomous command (example)
-    // if (m_autonomousCommand != null) {
-    //   m_autonomousCommand.start();
-    //  }
-    m_DriveWithJoysticks.start();
-    m_MoveElevator.start();
-  }
+    startCommands();
+   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
   @Override
   public void autonomousPeriodic() {
     Scheduler.getInstance().run();
-  }
+ }
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    // if (m_autonomousCommand != null) {
-    //   m_autonomousCommand.cancel();
-    // }
-    
-    m_DriveWithJoysticks.start();
-    m_MoveElevator.start();
-  }
+    startCommands();
+   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
-  }
+ }
 
-  /**
-   * This function is called periodically during test mode.
-   */
+  // This function is called periodically during test mode.
   @Override
   public void testPeriodic() {
+  }
+
+  // Starts up all commands once 
+  private void startCommands() {
+    if (!commandsStarted) {
+      m_DriveWithJoysticks.start();
+      m_MoveElevator.start();
+      // m_RunWrist.start();
+
+      commandsStarted = true;
+    }
   }
 }
