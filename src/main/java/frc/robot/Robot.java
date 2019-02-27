6636a009974e34ab681875ebcb47e02d3f832b7c
Mark Bushman <m.bushman@cox.net>
2019-02-27 01:29:10 -0500

    experimenting with camera settings to reduce lag.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,13 +6,21 @@
 /*----------------------------------------------------------------------------*/
 package frc.robot;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+ 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.commands.DriveWithJoysticks;
 import frc.robot.commands.MoveElevator;
-//import frc.robot.commands.RunWrist;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,6 +30,7 @@ import frc.robot.commands.MoveElevator;
  * project.
  */
 public class Robot extends TimedRobot {
+  Thread m_visionThread;
   static OI oi = new OI();
   static boolean commandsStarted = false;
 
@@ -29,15 +38,57 @@ public class Robot extends TimedRobot {
   // SendableChooser<Command> m_chooser = new SendableChooser<>();
   Command m_DriveWithJoysticks;
   Command m_MoveElevator;
-  Command m_RunWrist;
 
   @Override
   public void robotInit() {
-    // Start the camera server
-    CameraServer.getInstance().startAutomaticCapture();
+    m_visionThread = new Thread(() -> {
+      // Get the UsbCamera from CameraServer
+      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+      // Set the resolution
+      camera.setResolution(1280, 720);
+
+      // Get a CvSink. This will capture Mats from the camera
+      CvSink cvSink = CameraServer.getInstance().getVideo();
+      // Setup a CvSource. This will send images back to the Dashboard
+      CvSource outputStream
+          = CameraServer.getInstance().putVideo("Rectangle", 320, 180);
+
+      // Mats are very memory expensive. Lets reuse these Mats.
+      Mat matIn  = new Mat();
+      Mat matOut = new Mat();
+      Mat matSize = new Mat();
+      Size scaleSize = new Size(320,180);
+
+      // This cannot be 'true'. The program will never exit if it is. This
+      // lets the robot stop this thread when restarting robot code or
+      // deploying.
+      while (!Thread.interrupted()) {
+        // Tell the CvSink to grab a frame from the camera and put it
+        // in the source mat.  If there is an error notify the output.
+        if (cvSink.grabFrame(matIn) == 0) {
+          // Send the output the error.
+          outputStream.notifyError(cvSink.getError());
+          // skip the rest of the current iteration
+          continue;
+        }
+        // Put a rectangle on the image
+        // Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),new Scalar(255, 255, 255), 5);
+      
+        // Convert the image to gray scale (hoping to save bandwidth)
+        Imgproc.cvtColor(matIn, matOut, Imgproc.COLOR_BGR2GRAY);
+       
+        // Convert the size of the image (again hoping to save bandwith)
+        Imgproc.resize(matOut, matSize, scaleSize, 0, 0, Imgproc.INTER_NEAREST);
+        
+        // Give the output stream a new image to display
+        outputStream.putFrame(matSize);
+      }
+    });
+    m_visionThread.setDaemon(true);
+    m_visionThread.start();
+
     m_DriveWithJoysticks = new DriveWithJoysticks();
     m_MoveElevator = new MoveElevator();
-    // m_RunWrist = new RunWrist();
   }
 
   @Override
@@ -82,7 +133,6 @@ public class Robot extends TimedRobot {
     if (!commandsStarted) {
       m_DriveWithJoysticks.start();
       m_MoveElevator.start();
-      // m_RunWrist.start();
 
       commandsStarted = true;
     }
