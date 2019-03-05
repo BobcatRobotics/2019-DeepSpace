ecec0a9bd142695835f67b4fba8da04a755e4871
Mark Bushman <m.bushman@cox.net>
2019-03-05 06:56:37 -0500

    Elevator 30% when locked, cam changes (needs update) -mab

--- src/main/java/frc/robot/commands/MoveElevator.java
+++ src/main/java/frc/robot/commands/MoveElevator.java
@@ -21,12 +21,20 @@ public class MoveElevator extends Command {
         double elev = OI.gamePad.getRawAxis(RobotMap.gamePadLeftPwrStick);
         double motorSpeed =  elev;
         if (!OI.elev1.isLimDisable()) {
-            if (OI.elev1.lowerLimit()) {
-
-                if (motorSpeed > 0.0) {
-                    motorSpeed = 0.0;
-                }
-            }
+            if (OI.lock.isLocked()) {
+                if (OI.elev1.lowerLimit()) {
+                    if (motorSpeed > 0.3) {
+                           motorSpeed = 0.3;
+                       }
+                  }
+   
+            } else {
+                if (OI.elev1.lowerLimit()) {
+                    if (motorSpeed > 0.0) {
+                           motorSpeed = 0.0;
+                       }
+                  }
+           }
 
             if (OI.elev1.upperLimit()) {
             
