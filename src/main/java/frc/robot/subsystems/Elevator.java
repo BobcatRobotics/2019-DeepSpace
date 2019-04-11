3bf678f90721faa7f653c79e101fcaacf979ed3e
Mark Bushman <m.bushman@cox.net>
2019-04-11 12:59:21 -0400

    Update bias for 2nd stage elastics

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -16,7 +16,8 @@ import frc.robot.lib.RioLogger;
 
 
 public class Elevator extends Subsystem {
-    private double elevBiasDefault = -0.05;  // Competition was -0.04 at Waterbury
+    private double elevBiasDefault = -0.08;  // Competition was -0.04 at Waterbury
+                                             // -.05 ar hrtfrd,  -0.08 at ne champs with elastic
     private double elevScaleDefault = 1.0;   // Competition is 0.6 at Waterbury
     private boolean elevLimDisDef = false;
     private double elevUpperLimit = 42000;    // Competiton bot upper limit was 39500 is Wtbry
